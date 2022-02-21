22878b27547c1edaf21c753de8ced8293191614a
Manuel Casado <ManuelCasado@protonmail.com>
2022-02-21 13:08:19 +0100

    Ejercicio5 resuelto

--- src/for and if/exercise5/index.js
+++ src/for and if/exercise5/index.js
@@ -2,5 +2,10 @@ const numbers = [0,1,5,2,7,5,0,0,9,5,5,6];
 let counter = 0;
 
 // Put your code here
+for (item of numbers){
+    if (item===5){
+        counter++
+    }
+}
 
-console.log(counter)
+console.log("El número 5 aparece en el array",counter, "veces");
