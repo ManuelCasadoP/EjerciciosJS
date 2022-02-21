e45b5da5e0d80982bb3053574cd654dc873ed5ac
Manuel Casado <ManuelCasado@protonmail.com>
2022-02-21 12:46:38 +0100

    Ejercicio2 resuelto

--- src/for and if/exercise2/index.js
+++ src/for and if/exercise2/index.js
@@ -1,8 +1,9 @@
 const numbers = [0,1,2,4,5,9,3,6,7,8];
 
-// Put your code here
+counter=0; // Put your code here
 for (let item of numbers) {
     // Put your code here
+    counter++;
 }
 
-console.log(/* Put your code here */)
+console.log("El número de elementos del array es ", counter/* Put your code here */)
