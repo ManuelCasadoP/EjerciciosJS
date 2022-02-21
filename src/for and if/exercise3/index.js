cf46de213af31bf9bc6adb73868b530973880f41
Manuel Casado <ManuelCasado@protonmail.com>
2022-02-21 12:57:35 +0100

    Ejercicio3 resuelto

--- src/for and if/exercise3/index.js
+++ src/for and if/exercise3/index.js
@@ -3,5 +3,10 @@ let sum = 0;
 let average = 0;
 
 // Put your code here
-
-console.log(average)
+for (let item of numbers) {
+    sum = sum + item;
+    average++;
+}
+console.log("La suma de elementos del array es :", sum)
+console.log("Cantidad de elementos del array es :", average);
+console.log("La media aritmética de elementos del array es :", sum/average);
