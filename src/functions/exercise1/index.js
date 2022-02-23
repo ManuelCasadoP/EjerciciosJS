46efe8fd69c0dc698daf77d95af98488bc94e4cd
Manuel Casado <ManuelCasado@protonmail.com>
2022-02-23 13:06:00 +0100

    Ejercicio1 Resuelto

--- src/functions/exercise1/index.js
+++ src/functions/exercise1/index.js
@@ -6,11 +6,13 @@ const numbers4 = [22,9,6,8,5,7,8,9,12,11];
 function minorValue(array) {
 
     // Put your code here
+    array.sort(function(a, b){return a - b});
+    return array[0];
 
 }
 
 const minor1 = minorValue(numbers1);
-console.log(minor1);
-console.log(minorValue(numbers2));
-console.log(minorValue(numbers3));
-console.log(minorValue(numbers4));
+console.log('El número menor del Array "Numbers1" es :', minor1);
+console.log('El número menor del Array "Numbers2" es :', minorValue(numbers2));
+console.log('El número menor del Array "Numbers3" es :', minorValue(numbers3));
+console.log('El número menor del Array "Numbers4" es :', minorValue(numbers4));
