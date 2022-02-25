7ba476b6c50b412028a8e7ed6a08627fc7bf2627
Manuel Casado <ManuelCasado@protonmail.com>
2022-02-25 10:03:12 +0100

    Se añade comentario sobre la otra opción de console.log

--- src/for and if/exercise8/index.js
+++ src/for and if/exercise8/index.js
@@ -8,5 +8,6 @@ for (let outer = 1; outer <= 9 ; outer++) {
     for (let inner = 0; inner <= 9; inner++ ){ 
         
         console.log(outer,"x", inner, "=" , outer*inner);
+      //console.log(`Outer: ${outer}, Inner: ${inner}`);  
     }
 }
