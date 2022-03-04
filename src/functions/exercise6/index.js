99d49706dd24b47edd422bc325c30df6df8eae0d
Manuel Casado <ManuelCasado@protonmail.com>
2022-03-04 08:58:28 +0100

    Añadidas funciones reutilizadas

--- src/functions/exercise6/index.js
+++ src/functions/exercise6/index.js
@@ -2,6 +2,25 @@ const numbers = [11,9,13,12]
 
 // Paste your functions here
 
+function swap(idx1,idx2,array){
+    let temporal=array[idx1]
+    array[idx1]=array[idx2];
+    array[idx2]=temporal;
+}
+
+function minorInRange(idx1,idx2,array){
+    let numMenor=null;
+    for(i=idx1; i<=idx2; i++){
+        if (array[idx1] < array[idx1+1]){
+            numMenor=idx1;
+        }
+        else {
+            numMenor=idx1+1;
+        }
+    }
+    return numMenor;
+}
+
 // Put your code here
 
 console.log(numbers);
